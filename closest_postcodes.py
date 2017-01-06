0a6fec2df0ca7b4665bc69796c4d72f79c695931
Vasilis Oikonomou <v.oikonomou@berkeley.edu>
2017-01-06 16:40:02 +0000

    optimized way of finding closest post codes

--- closest_postcodes.py
+++ closest_postcodes.py
@@ -4,16 +4,21 @@ import numpy as np
 
 zips = pd.read_csv('zips_coordinates.csv')
 
+
 def eu(l1, l2):
     return np.sqrt(sum((np.array(l1)-np.array(l2))**2))
 
 def k_closest_postcodes(k, zipcode):
+    
     if zipcode not in list(zips['pcd']):
 	     raise ValueError("post code not found")
     coords = zips[zips['pcd']==zipcode]
     coords = np.array(coords.iloc[0])[1:]
+    lat = coords[0]
+    lon = coords[1]
+    filtered = zips[(zips['lat'] > lat-0.05) & (zips['lat'] < lat+0.05) & (zips['long'] > lon-0.05) & (zips['long'] < lon+0.05)]
     dist = []
-    for i, j in zip(zips['lat'], zips['long']):
+    for i, j in zip(filtered['lat'], filtered['long']):
         dist.append(eu(coords, [i,j]))
-    zips['dist'] = dist
-    return list(zips.sort_values("dist", ascending=True)['pcd'])[:k]
+    filtered['dist'] = dist
+    return list(filtered.sort_values("dist", ascending=True)['pcd'])[:k]
